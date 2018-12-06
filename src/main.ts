0c905c17b735673a8b62757f4da13a117bd99423
chronos241 <chronos241@gmail.com>
2018-12-06 22:51:13 +0900

    modify screen resolution (for smartphone)

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import TestScene from './scenes/PlayScene';
 const config:GameConfig = {
     type: Phaser.AUTO,
     parent: 'content',
-    width: 640,
-    height: 480,
+    width: 540,
+    height: 960,
     resolution: 1, 
     backgroundColor: "#EDEEC9",
     scene: [
